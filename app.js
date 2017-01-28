fed5dee8b249012e2a9235d879089264c81b96c8
Shauan Ferreira Leite <shauanf@gmail.com>
2017-01-28 15:30:02 -0500

    add express-session, passport, new routes

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 // add new modules and files here
+//Passport middleware
+const session = require('express-session');
+const passport = require('passport');
+
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth.js');
+const userRoutes = require('./routes/user.js');
+const app = express();
+
+// load environment variables
+require('dotenv').config();
+// end new files part1
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,11 +30,22 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // add new express-session and passport middleware here
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // add route middleware here
-
+app.use('/', index);
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
